ca00ee4a4640674d2fed8085dde2be28d1d5a5d2
Chris Abbato <chrisabbato@gmail.com>
2021-03-16 21:42:40 -0500

    Adds breed search functionality

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DoggyApiService } from '../shared/doggy-api.service';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   title = 'Home';
+  breeds: string[] = [];
+  filteredBreeds: string[] = [];
 
-  constructor() {}
+  constructor(public doggyApi: DoggyApiService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.LoadBreeds();
+  }
+
+  LoadBreeds() {
+    return this.doggyApi.getBreeds().subscribe((data) => {
+      this.breeds = Array.from(Object.keys((data as any).message));
+      this.filteredBreeds = this.breeds;
+    });
+  }
+
+  filterBreeds(searchValue: string) {
+    this.filteredBreeds = this.breeds.filter((breed) =>
+      breed.toLowerCase().includes(searchValue.toLowerCase())
+    );
+  }
 }
